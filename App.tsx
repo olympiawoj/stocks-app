9a7bb2b516ac137dfcfa26524a4a0337a68714cc
olympiawoj <owojcik7@gmail.com>
2020-12-17 20:46:20 -0500

    Add initial ios stock design asset and render input to screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import {Hello} from "./components/Hello"
 import {Header} from './components/Header/Header'
 
 
 export default function App() {
+
+  const [value, onChangeText] = useState('')
 
   interface Person {
     firstName: string;
@@ -17,10 +19,13 @@ export default function App() {
 
   let user = {firstName: "Jane", lastName: "User"};
   console.log(greeter(user))
+
   
   return (
     <View style={styles.container}>
-      <Header date="September 10"/>
+      <Header date="September"/>
+      <TextInput  style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }} value={value} onChangeText={text => onChangeText(text)}/>
+      <Text>Hi</Text>
     </View>
   );
 }
