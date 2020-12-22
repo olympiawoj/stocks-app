b9fd10b00f14a48116f217bcf92d1dcb1a9a360f
olympiawoj <owojcik7@gmail.com>
2020-12-22 16:52:49 -0500

    Install axios and perform 1st HTTP request

--- App.tsx
+++ App.tsx
@@ -1,46 +1,61 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput} from 'react-native';
-import {Hello} from "./components/Hello"
-import {Header} from './components/Header/Header'
-import {colors} from './utils/colors'
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import { Hello } from "./components/Hello";
+import { Header } from "./components/Header/Header";
+import { colors } from "./utils/colors";
+import axios from "axios";
 
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 // @ts-ignore
-import { API_KEY} from 'react-native-dotenv'  
-
+import { API_KEY } from "react-native-dotenv";
 
 export default function App() {
-
-  const [value, onChangeText] = useState('')
-
+  const [value, onChangeText] = useState("");
 
   interface Person {
     firstName: string;
     lastName: string;
   }
 
-  function greeter(person:Person){
-    return "Hello " + person.firstName + " " + person.lastName
-  }
+  const searchValue = "AAPL";
+  const searchURL = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchValue}&apikey=${API_KEY}`;
+  axios
+    .get(searchURL)
+    .then((res) => console.log(res.data))
+    .catch((err) => console.log(err));
+  console.log(API_KEY);
+  console.log("hi");
 
-  let user = {firstName: "Jane", lastName: "User"};
-  console.log(greeter(user))
-
-  console.log(API_KEY)
-  console.log('hi')
-
-  
   return (
     <View style={styles.container}>
-      <Header date="September"/>
+      <Header date="September" />
 
-    <View style={{backgroundColor: colors.searchBackground, width: 275, borderRadius: 20, padding: 5}}>
-    <View style={{display: 'flex', flexDirection:  'row', alignItems: 'center'}}>
-     <FontAwesomeIcon icon={ faSearch } color={colors.gunsmokeGrey}/>
-      <TextInput  style={{ height: 35, width: 200, flex: 1, paddingLeft: 7}} value={value} onChangeText={text => onChangeText(text)} placeholder='Search' placeholderTextColor={colors.gunsmokeGrey}/>
-      </View>
+      <View
+        style={{
+          backgroundColor: colors.searchBackground,
+          width: 275,
+          borderRadius: 20,
+          padding: 5,
+        }}
+      >
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <FontAwesomeIcon icon={faSearch} color={colors.gunsmokeGrey} />
+          <TextInput
+            style={{ height: 35, width: 200, flex: 1, paddingLeft: 7 }}
+            value={value}
+            onChangeText={(text) => onChangeText(text)}
+            placeholder="Search"
+            placeholderTextColor={colors.gunsmokeGrey}
+          />
+        </View>
       </View>
     </View>
   );
@@ -49,8 +64,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0B0B0B',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#0B0B0B",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
