5bd46bf6e1316bd6367fb89b44572431f919ee22
olympiawoj <owojcik7@gmail.com>
2020-12-22 17:54:24 -0500

    Get data from handleSearch rendering to screen

--- App.tsx
+++ App.tsx
@@ -7,29 +7,30 @@ import axios from "axios";
 
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import {renameKeys} from './utils/renameKeys'
+import { renameKeys } from "./utils/renameKeys";
 
 // @ts-ignore
 import { API_KEY } from "react-native-dotenv";
 
 export default function App() {
   const [value, onChangeText] = useState("");
-  const [filteredOptions, setFilteredOptions] = useState([])
+  const [filteredOptions, setFilteredOptions] = useState([]);
 
   const searchValue = "AAPL";
   const searchURL = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${value}&apikey=${API_KEY}`;
 
-  const handleSearch = async (e:any) =>{
-    console.log('handleSearch running...')
+  const handleSearch = async (e: any) => {
+    console.log("handleSearch running...");
     try {
-      const data = await axios.get(searchURL)
-      console.log(data)
-      return data
+      const response = await axios.get(searchURL);
+      console.log(response.data["bestMatches"]);
+      if (response.data) {
+        setFilteredOptions(response.data["bestMatches"]);
+      }
+    } catch (error) {
+      console.log("error", error);
     }
-    catch(error){
-      console.log('error',error)
-    }
-  }
+  };
 
   return (
     <View style={styles.container}>
@@ -57,9 +58,17 @@ export default function App() {
             onChangeText={(text) => onChangeText(text)}
             placeholder="Search"
             placeholderTextColor={colors.gunsmokeGrey}
-            onSubmitEditing={e => handleSearch(e)}
+            onSubmitEditing={(e) => handleSearch(e)}
           />
         </View>
+        <Text style={{ color: "white" }}>List</Text>
+        {filteredOptions.map((option, idx) => {
+          return (
+            <>
+              <Text style={{ color: "white" }} key={idx}>{JSON.stringify(option)}</Text>
+            </>
+          );
+        })}
       </View>
     </View>
   );
