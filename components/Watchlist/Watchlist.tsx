f9ca31b3c8cfb865e62ddbf4dbe1bbae7cda5c0e
olympiawoj <owojcik7@gmail.com>
2021-02-02 20:38:26 -0500

    swiping is working

--- components/Watchlist/Watchlist.tsx
+++ components/Watchlist/Watchlist.tsx
@@ -1,76 +1,98 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
-import {Text, View} from 'react-native'
+import { Text, View } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {handleQuote, handleCompanyOverview} from '../../utils/api'
+import { handleQuote, handleCompanyOverview } from "../../utils/api";
 import { colors } from "../../utils/colors";
+import Swipeable from "react-native-gesture-handler/Swipeable";
 
 interface Watchlist {
   ticker: string;
   price: string;
   name: string;
-  [props: string]: string
+  [props: string]: string;
 }
-export const Watchlist = ()=>{
+export const Watchlist = () => {
+  const [myWatchlist, setMyWatchlist] = useState<Watchlist[]>([]);
 
-  const [myWatchlist, setMyWatchlist] = useState<Watchlist[]>([])
-
-
-  useEffect(()=>{
-    setMyWatchlist([])
+  useEffect(() => {
+    setMyWatchlist([]);
     const getData = async () => {
       try {
-        const watchlist = await AsyncStorage.getItem('watchlist')
-        if(watchlist !== null) {
+        const watchlist = await AsyncStorage.getItem("watchlist");
+        if (watchlist !== null) {
           // value previously stored
-          console.log('value in asyncstorage', watchlist)
-          let watchlistArr= JSON.parse(watchlist) 
-          let watchlistPricesArr:Watchlist[] = [] 
-          watchlistArr.forEach((async(ticker:string) => {
-           try {
-            const price = await handleQuote(ticker)
-            const companyOverview = await handleCompanyOverview(ticker) 
-            const newStockObj = {ticker, price, name: companyOverview["Name"]} as Watchlist
-            //@ts-ignore
-            setMyWatchlist(myWatchlist => [...myWatchlist, newStockObj])
-           }
-           catch(err) {
-             console.log(err)
-           }
-          }))
-          console.log('arr', myWatchlist)
-
+          console.log("value in asyncstorage", watchlist);
+          let watchlistArr = JSON.parse(watchlist);
+          let watchlistPricesArr: Watchlist[] = [];
+          watchlistArr.forEach(async (ticker: string) => {
+            try {
+              const price = await handleQuote(ticker);
+              const companyOverview = await handleCompanyOverview(ticker);
+              const newStockObj = {
+                ticker,
+                price,
+                name: companyOverview["Name"],
+              } as Watchlist;
+              //@ts-ignore
+              setMyWatchlist((myWatchlist) => [...myWatchlist, newStockObj]);
+            } catch (err) {
+              console.log(err);
+            }
+          });
+          console.log("arr", myWatchlist);
         }
-          
-      } catch(e) {
+      } catch (e) {
         // error reading value
       }
-    }
-    getData()
-    
+    };
+    getData();
+  }, []);
 
-  },[])
+
 
   return (
-    <View>{myWatchlist.length > 0  && myWatchlist.map(stock => {
-      return (
-          <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            padding: 10,
-            width: 300,
-            height: 75
-          }}
-        > 
-        <View>
-        <Text style={{ color: "white", fontSize: 17}}>{stock.ticker}</Text>
-        <Text key={stock.name} style={{color: colors.gunsmokeGrey}}>{stock.name}</Text>
-        </View>
-        <Text style={{ color: "white" }}>{stock.price?.slice(0, -2)}</Text>
-        </View>
-      )
-    })}</View>
-  )
-}
+    <View>
+      {myWatchlist.length > 0 &&
+        myWatchlist.map((stock) => {
+          return (
+            <Swipeable
+            
+            friction={2}
+            leftThreshold={80}
+            onSwipeableOpen={() => console.log('swipe opening')}
+            renderRightActions={()=>(
+              <View style={{backgroundColor: 'red'}}>
+                <Text>Remove</Text>
+              </View>
+            )}
+            >
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignContent: 'center',
+                  padding: 10,
+                  width: 300,
+                  height: 100,
+                }}
+              >
+                <View>
+                  <Text style={{ color: "white", fontSize: 17 }}>
+                    {stock.ticker}
+                  </Text>
+                  <Text key={stock.name} style={{ color: colors.gunsmokeGrey }}>
+                    {stock.name}
+                  </Text>
+                </View>
+                <Text style={{ color: "white" }}>
+                  {stock.price?.slice(0, -2)}
+                </Text>
+              </View>
+            </Swipeable>
+          );
+        })}
+    </View>
+  );
+};
